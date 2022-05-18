b117be29a176a3d91ffc9bc21f27494f812d1e21
listonaj <jm.romain@outlook.com>
2022-05-18 08:22:27 -0700

    comments added on code

--- theWalkingDead/models.py
+++ theWalkingDead/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create a class Topic that inherit from Model (parent class included in Django).
 class Topic(models.Model):
     """A topic the user is learning about."""
+    # two attributes: txt and date
+    # the text is of CharField attribute - character attributes (
+    # used for small amount of character : 200 characters)
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
